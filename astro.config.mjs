49ec827223553e41f069a5d5728cceb775c65da1
vega <vegalin1029@gmail.com>
2026-02-01 12:40:27 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 import { siteConfig } from './site.config';
 
 export default defineConfig({
   site: siteConfig.url,
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react()],
   markdown: {
     shikiConfig: { theme: 'github-light' },
   },
