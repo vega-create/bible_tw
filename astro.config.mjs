5e0600afeae038a2f51857c09f37be326d91554c
vega <vegalin1029@gmail.com>
2026-02-09 15:49:20 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import sitemap from '@astrojs/sitemap';
 import { siteConfig } from './site.config';
 
 export default defineConfig({
